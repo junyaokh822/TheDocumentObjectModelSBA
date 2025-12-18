c1a4161efccfd7408206ae96ff0498b8a9a3032c
Junyao <junyaokh822@gmail.com>
2025-12-18 17:36:28 -0500

    added item template

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
 const formEl = document.getElementById("item-form");
 const titleEl = document.getElementById("title");
 const inventoryEl = document.getElementById("inventory");
+
+const clearBtn = document.querySelector("#clear");
+const itemTemplate = document.querySelector("#item-template");
