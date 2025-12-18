25211feee6ead8d7ea23b47fd95adfbcc8367b2f
Junyao <junyaokh822@gmail.com>
2025-12-18 17:53:20 -0500

    apply appendchild

--- script/script.js
+++ script/script.js
@@ -9,3 +9,8 @@ const subtitle = document.createElement("p");
 subtitle.textContent = "Add items to warehouse";
 document.body.insertBefore(subtitle, formEl);
 
+const totalEl = document.createElement("div");
+totalEl.id = "total";
+totalEl.textContent = "Total items: 0";
+inventoryEl.appendChild(totalEl);
+
