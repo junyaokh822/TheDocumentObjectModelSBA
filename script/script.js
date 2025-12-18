ee6ada5e4c6d7d291c374aa4c0871c08d08aca08
Junyao <junyaokh822@gmail.com>
2025-12-18 17:49:01 -0500

    display added items text

--- script/script.js
+++ script/script.js
@@ -4,3 +4,8 @@ const inventoryEl = document.getElementById("inventory");
 
 const clearBtn = document.querySelector("#clear");
 const itemTemplate = document.querySelector("#item-template");
+
+const subtitle = document.createElement("p");
+subtitle.textContent = "Add items to warehouse";
+document.body.insertBefore(subtitle, formEl);
+
