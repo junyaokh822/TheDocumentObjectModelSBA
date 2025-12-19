3ebb003473f93d802ffb0660a81e723fae0a44e6
Junyao <junyaokh822@gmail.com>
2025-12-19 12:58:44 -0500

    added documentFragment

--- script/script.js
+++ script/script.js
@@ -24,3 +24,45 @@ clearBtn.addEventListener("click", clearForm);
 
 //event based validation
 document.getElementById("location").addEventListener("input", validateLocation);
+
+//BOM properties/method
+console.log("Page URL:", window.location.href); //BOM property
+
+function useConfirm() {
+  return window.confirm("Are you sure?"); //BOM method
+}
+
+//documentFragment for templating
+let items = []; //array to store warehouse items
+let nextId = 1; //unique id given to each item
+
+function addItem(name, quantity, location) {
+  const fragment = new DocumentFragment(); //create an documentfragment
+  const clone = itemTemplate.textContent.cloneNode(true); //gets the content inside <template> and clone them
+
+  //fill the cloned template with data
+  clone.querySelector(".item-name").textContent = name;
+  clone.querySelector(".item-qty").textContent = quantity;
+  clone.querySelector(".item-loc").textContent = location || "N/A";
+
+  //add event listener to delete button
+  const deleteBtn = clone.querySelector(".delete");
+  deleteBtn.addEventListener("click", function () {
+    if (useConfirm()) {
+      // show confirmation dialog
+      deleteItem(this.parentElement); // delete the item's parent div
+    }
+  });
+
+  const editBtn = clone.querySelector(".edit");
+  //add event listener to edit button
+  editBtn.addEventListener("click", function () {
+    editItem(this.parentElement); //edit the item's parent div
+  });
+
+  fragment.appendChild(clone); //add clone to the documentFragment
+  inventoryEl.appendChild(fragment); //add the fragment to the page DOM
+
+  items.push({ id: nextId++, name, quantity, location }); //add item data to the array
+  updateDisplay(); //refresh total counts
+}
